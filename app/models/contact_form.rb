9948d1fd46a11f3f87c23fb66ab2b57712cd9f46
Sam Mayhew <mayhew.sam@yahoo.com>
2023-10-04 18:52:49 +0100

    done some stuff but not sure what

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,2 +1,5 @@
 class ContactForm < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :message, presence: true, length: { minimum: 10 }
 end
